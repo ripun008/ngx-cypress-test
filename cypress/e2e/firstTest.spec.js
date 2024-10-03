bd3b08f86780a9fa7c891d7072fb2a429a967dab
Ripunjaya Saran <ripun008@gmail.com>
2024-10-03 18:08:10 -0400

    Add lists and dropdown coverage

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -150,19 +150,55 @@ describe('First test suite', () => {
         cy.contains('Forms').click()
         cy.contains('Datepicker').click()
 
-        let date = new Date()
-        date.setDate(date.getDate() + 5)
-        let futureDate = date.getDate()
-        let dateToAssert = `Oct ${futureDate}, 2024`
-
         cy.contains('nb-card', 'Common Datepicker').find('input').then( input => {
             cy.wrap(input).click()
-            cy.get('.day-cell').not('.bounding-month').contains(futureDate).click()
+            const dateToAssert = selectDayFromCurrent(5)
             cy.wrap(input).invoke('prop', 'value').should('equal', dateToAssert)
             cy.wrap(input).should('have.value', dateToAssert)
-        })        
-
+        })  
         
+        function selectDayFromCurrent(day) {
+            let date = new Date()
+            date.setDate(date.getDate() + day)
+            let futureDay = date.getDate()
+            let futureMonth = date.toLocaleDateString('en-US', {month: 'short'})
+            let futureYear = date.getFullYear()
+            cy.log('futureDay=',{futureDay})
+            cy.log('futureMonth=',{futureMonth})
+            cy.log('futureYear=',{futureYear})
+            let dateToAssert = `${futureMonth} ${futureDay}, ${futureYear}`
 
+            cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then( dateAttribute => {
+                if(!dateAttribute.includes(futureMonth) || !dateAttribute.includes(futureYear)) {
+                    cy.get('[data-name="chevron-right"]').click()
+                    selectDayFromCurrent(day)
+                } else {
+                    cy.get('.day-cell').not('.bounding-month').contains(futureDay).click()
+                }
+            })
+            return dateToAssert
+        }
+    })
+
+    it.only('Lists and dropdowns', () => {
+        cy.visit('/') // visits baseURL defined in cypress.config file
+
+        // select an item from the list
+        cy.get('nav nb-select').click()
+        cy.get('.options-list').find('nb-option').contains('Dark').click()
+        cy.get('nav nb-select').should('contain', 'Dark')
+
+        // iterate through the list using the each() method
+        cy.get('nav nb-select').then( dropDown => {
+            cy.wrap(dropDown).click()
+            cy.get('.options-list nb-option').each( (listItem, index) => {
+                const itemText = listItem.text().trim()
+                cy.wrap(listItem).click()
+                cy.wrap(dropDown).should('contain', itemText)
+                if(index < 3) {
+                    cy.wrap(dropDown).click()
+                }
+            })
+        })
     })
 })
